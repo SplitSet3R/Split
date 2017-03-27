7e19c4cc1cfc0747a1c00529257269c3f8f59994
Matthew Li <li.matthew.m@gmail.com>
2017-03-26 18:02:37 -0700

    Search working, still fixing AJAX add

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
-    //
+    /*
+     * Returns query results based on the search request.
+     * Exludes the user who is currently authenticated from
+     * search results.
+     */
+    public function search(Request $req) {
+        if (isset($req->search)) {
+            $query = trim($req->search);
+            $search_users = User::where('username', 'like', '%' . $query . '%')
+                ->where('username', '!=', Auth::user()->username)
+                ->get();
+            return view('search-friends', compact('search_users'));
+        }
+        return view('search-friends');
+    }
+
+    /*
+     * Returns the default view of the search page.
+     */
+    public function index() {
+        return view('search-friends');
+    }
 }
