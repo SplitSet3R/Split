dc0df3c65d10bb154cd9eb346f7b4d9a48807f1d
Vincent Lee <vincent.h.lee@outlook.com>
2017-03-27 21:25:18 -0700

    correct table inputs

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -25,34 +25,5 @@
                     <p>add friend</p>
                 </a>
             </li>
-            <li>
-                <!-- Testing notifications here -->
-                <a href="#expenseNotifications" data-toggle="collapse">
-                    <i class="material-icons">content_paste</i>
-                    <p>Expense Notifications</p>
-                </a>
-                <div class="collapse" id="expenseNotifications">
-                    <ul>
-                        @foreach($expenseNotifications as $expenseNotification)
-                            <li>{{$expenseNotification[1]}}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </li>
-            <li>
-                <!-- Testing notifications here -->
-                <a href="#friendNotifications" data-toggle="collapse">
-                    <i class="material-icons">content_paste</i>
-                    <p>Friend Notifications</p>
-                </a>
-                <div class="collapse" id="friendNotifications">
-                    <ul>
-                        @foreach($friendNotifications as $friendNotification)
-                            <li>{{$friendNotification[1]}}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </li>
-
         </ul>
     </div>
