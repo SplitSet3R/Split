6d7754f1b4d2e7931631c146d03999cdce3ba764
Morris Arroyo <arroyomorris@yahoo.ca>
2017-04-02 11:52:20 -0700

    finished GroupCreateController - clean, unchecked;

--- app/Http/Controllers/GroupCreateController.php
+++ app/Http/Controllers/GroupCreateController.php
@@ -2,49 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\GroupMember;
+
 use Illuminate\Http\Request;
 
 class GroupCreateController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-/*
-    public function addfriend(Request $req) {
-        if ($req->json() && isset($req->username)) {
-            $friendstatus = Friend::whereIn('username1', [Auth::user()->username, $req->username])
-                ->whereIn('username2', [Auth::user()->username, $req->username])
-                ->count();
-            if ($friendstatus < 1) { // Authenticated user is not friends with $req->username
-                $friendship = new Friend;
-                $friendship->username1 = Auth::user()->username;
-                $friendship->username2 = $req->username;
-                $friendship->status_code = 'pending';
-                $friendship->action_username = Auth::user()->username;
-                $friendship->save();
-                return response()->json(array("message" => "friend request sent!"), 200);
-            }
-        }
-        return response()->json(array("message" => "generic error message"), 500);
-    }
-*/
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -71,57 +34,14 @@ class GroupCreateController extends Controller
                 $groupMember = new GroupMember;
                 $groupMember->group_id = $group->id;
                 $groupMember->username = $req->groupMembers[$i]->username;
-                $groupMember->status_code = 'pending'; // have to change 'accepted' into a constant
+                $groupMember->status_code = 'pending'; // have to change 'pending' into a constant
                 $groupMember->action_group_id = #####; // have to figure out what this is
                 $groupMember->is_Admin = 0;
                 $groupMember->save();
 
             }
+            return; // need to return somthing
         }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return; // need to return somthing
     }
 }
