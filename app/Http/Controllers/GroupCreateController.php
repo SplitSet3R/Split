f39f705dc03e130b2911b8cbbb5059bf08ad4376
Morris Arroyo <arroyomorris@yahoo.ca>
2017-04-02 11:48:00 -0700

    finished GroupCreateController - unclean; created GroupMembers model; add $fillable to Group model

--- app/Http/Controllers/GroupCreateController.php
+++ app/Http/Controllers/GroupCreateController.php
@@ -16,6 +16,25 @@ class GroupCreateController extends Controller
         //
     }
 
+/*
+    public function addfriend(Request $req) {
+        if ($req->json() && isset($req->username)) {
+            $friendstatus = Friend::whereIn('username1', [Auth::user()->username, $req->username])
+                ->whereIn('username2', [Auth::user()->username, $req->username])
+                ->count();
+            if ($friendstatus < 1) { // Authenticated user is not friends with $req->username
+                $friendship = new Friend;
+                $friendship->username1 = Auth::user()->username;
+                $friendship->username2 = $req->username;
+                $friendship->status_code = 'pending';
+                $friendship->action_username = Auth::user()->username;
+                $friendship->save();
+                return response()->json(array("message" => "friend request sent!"), 200);
+            }
+        }
+        return response()->json(array("message" => "generic error message"), 500);
+    }
+*/
     /**
      * Show the form for creating a new resource.
      *
@@ -23,18 +42,42 @@ class GroupCreateController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request  $req
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $req)
     {
-        //
+        if ($req->json() && isset($req->name)) {
+            $group = new Group; // probably auto creates id
+            $group->name = $req->name;
+            $group->description = $req->description;
+            $group->save();
+            // admin could be first entry in group members input table (ie. can be included in the for loop)
+                // unmodifiable on front end
+            $adminGroupMember = new GroupMember;
+            $adminGroupMember->group_id = $group->id;
+            $adminGroupMember->username = Auth::user()->username; // couldn't test Auth::user()->username; Auth::user returns null
+            $adminGroupMember->status_code = 'accepted'; // have to change 'accepted' into a constant
+            $adminGroupMember->action_group_id = #####; // have to figure out what this is
+            $adminGroupMember->is_Admin = 1;
+            $adminGroupMember->save();
+            for ($i = 0; $i < sizeof($req->groupMembers); $i++) {
+                $groupMember = new GroupMember;
+                $groupMember->group_id = $group->id;
+                $groupMember->username = $req->groupMembers[$i]->username;
+                $groupMember->status_code = 'pending'; // have to change 'accepted' into a constant
+                $groupMember->action_group_id = #####; // have to figure out what this is
+                $groupMember->is_Admin = 0;
+                $groupMember->save();
+
+            }
+        }
     }
 
     /**
