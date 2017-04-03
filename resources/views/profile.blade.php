aeb0ce36123d2b2ee66aac7e939648974aa87d56
Daniel Capacio <danielcapacio@gmail.com>
2017-04-03 00:49:21 -0700

    Modal and edit button added for user profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -44,21 +44,53 @@
                                 <td>{{ Auth::user()->email }}</td>
                             </tr>
                             <tr class="active">
-                                <td>Active expenses: </td>
-                                <td></td>
-                            </tr>
-                            <tr class="active">
-                                <td>Total expenses: </td>
-                                <td></td>
-                            </tr>
-                            <tr class="active">
-                                <td>Total friends: </td>
-                                <td></td>
+                                <td>Biography: </td>
+                                <td>{{ Auth::user()->bio }}</td>
                             </tr>
                         </table>
+                        <button class="btn btn-success openEditProfileModal" data-toggle="modal" data-target="#editProfileModal">Edit Profile</button>
                     </div>
                 </div> <!-- row -->
             </div> <!-- container-fluid -->
         </div> <!-- main-panel -->
     </div> <!-- wrapper -->
-@endsection
+
+    <div class="modal fade openEditProfile" id="editProfileModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="editProfileLabel">Edit Profile</h4>
+                </div>
+                <form id="editProfileForm" action="/profile/{{ Auth::user()->username }}/edit" method="POST" class="form-horizontal">
+                    {{csrf_field()}}
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                {!! Form::label('username', 'Username:') !!}
+                                <input type="text" name="username" id="modal_username" class="form-control" value="{{ Auth::user()->username }}" readonly="true">
+
+                                {!! Form::label('firstname', 'First name:') !!}
+                                <input type="text" name="firstname" id="modal_firstname" class="form-control" value="{{ Auth::user()->firstname }}">
+
+                                {!! Form::label('lastname', 'Last name:') !!}
+                                <input type="text" name="lastname" id="modal_lastname" class="form-control" value="{{ Auth::user()->lastname }}">
+
+                                {!! Form::label('email', 'Email:') !!}
+                                <input type="email" name="email" id="modal_email" class="form-control" value="{{ Auth::user()->email }}">
+
+                                {!! Form::label('bio', 'Biography:') !!}
+                                <input type="text" name="bio" id="modal_bio" class="form-control" value="{{ Auth::user()->bio }}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        {!! Form::submit('Save',['class'=> 'btn btn-success', 'id' => 'editProfileBtn']) !!}
+                        <button type="button" id="closeEditProfileBtn" class="btn btn-warning" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+             </div>
+         </div>
+     </div>
+ @endsection
